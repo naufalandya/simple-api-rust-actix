3a75f195e9257cb85a6c33adbb2f9347a9143cb8
naufalandya <135545118+naufalandya@users.noreply.github.com>
2024-11-04 13:43:08 +0700

    ahaha

--- src/main.rs
+++ src/main.rs
@@ -23,22 +23,30 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         // Konfigurasi CORS
+        // let cors = Cors::default()
+        //     .allow_any_origin() // Mengizinkan semua origin. Untuk produksi, sebaiknya tentukan origin spesifik menggunakan `.allowed_origin("http://example.com")`
+        //     .allowed_methods(vec!["GET", "POST", "PUT", "DELETE", "OPTIONS"]) // Metode HTTP yang diizinkan
+        //     .allowed_headers(vec![
+        //         actix_web::http::header::AUTHORIZATION,
+        //         actix_web::http::header::CONTENT_TYPE,
+        //     ]) // Header yang diizinkan
+        //     .max_age(3600); // Durasi cache preflight request dalam detik
         let cors = Cors::default()
-            .allow_any_origin() // Mengizinkan semua origin. Untuk produksi, sebaiknya tentukan origin spesifik menggunakan `.allowed_origin("http://example.com")`
-            .allowed_methods(vec!["GET", "POST", "PUT", "DELETE", "OPTIONS"]) // Metode HTTP yang diizinkan
-            .allowed_headers(vec![
-                actix_web::http::header::AUTHORIZATION,
-                actix_web::http::header::CONTENT_TYPE,
-            ]) // Header yang diizinkan
-            .max_age(3600); // Durasi cache preflight request dalam detik
-
+        .allowed_origin_fn(|_, _| true)
+        .allowed_methods(vec!["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+        .allowed_headers(vec![
+            actix_web::http::header::AUTHORIZATION,
+            actix_web::http::header::CONTENT_TYPE,
+        ])
+        .max_age(3600);
+    
         App::new()
             .wrap(cors) // Menambahkan middleware CORS
             .wrap(Logger::default()) // Middleware Logger
             .app_data(actix_web::web::Data::new(pool.clone())) // Menambahkan pool ke data aplikasi
             .configure(routes::init_routes) // Mengatur semua route
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
